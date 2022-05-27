ce54fa4681d2174361943c5aa0288241d5b737c5
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-05-27 15:48:56 -0500

    Added unzip function

--- phone_num_search.py
+++ phone_num_search.py
@@ -1,5 +1,41 @@
 import re
 import os
+import logging
+from pathlib import Path
+from shutil import unpack_archive
+
+def main():
+
+    results = []
+    pattern = r"\d{3}-\d{3}-\d{4}"
+
+    unzipper()
+
+    for folder,subs,files in os.walk(os.getcwd() + "/zip_files"):
+        for file in files:
+            if file.endswith('.txt'):
+                full_path = folder + '/' + file
+                results.append(search(full_path, pattern))
+            else:
+                pass
+    for res in results:
+        if res != None:
+            print(res.group())
+
+def unzipper():
+    zip_dir = Path(os.getcwd() + "/zip_files/")
+    zip_files = zip_dir.rglob("*.zip")
+
+    while True:
+        try:
+            path = next(zip_files)
+        except StopIteration:
+            break  # no more files
+        except PermissionError:
+            logging.exception("permission error")
+        else:
+            extract_dir = path.with_name(path.stem)
+            unpack_archive(str(path), str(extract_dir), "zip")
 
 def search(file, pattern):
     f = open(file, 'r')
@@ -10,20 +46,6 @@ def search(file, pattern):
         return match
     else:
         pass
-
-
-def main():
-
-    results = []
-    pattern = r'\d{3}-\d{3}-\d{4}'
-
-    for folder, subs, files in os.walk(os.getcwd() + '/extracted_content'):
-        for f in files:
-            full_path = folder + '/' + f
-            results.append(search(full_path, pattern))
-    for r in results:
-        if r != None:
-            print(r.group())
 
 if __name__ == '__main__':
     main()
